92ff2080a89ede38ab393924efadee5f75345137
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-07-23 03:19:28 +0100

    ....

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // simple alert method
 
-alert("i love javaScript")
-document.write;
+// alert("i love javaScript")
+// document.write;
 
 // variables are containers of data.. data wo jw change hoa skay usay var kahtay hai.
 
@@ -9,3 +9,4 @@ var a = 2
 var b = 3
 var c = a + b
 console.log(c) // 5
+
