eb20282d6ed023abb0f621aaa626606261045a2d
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-07-23 03:24:01 +0100

    cont

--- app.js
+++ app.js
@@ -5,8 +5,14 @@
 
 // variables are containers of data.. data wo jw change hoa skay usay var kahtay hai.
 
-var a = 2
-var b = 3
-var c = a + b
-console.log(c) // 5
+// var a = 2
+// var b = 3
+// var c = a + b
+// console.log(c) // 5
+
+var a = 6;
+var b = 4;
+var c = a * b;
+console.log(c)cont
+
 
