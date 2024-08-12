cd278903e57701ae8f111471e0f3063c6f169b30
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-08-13 03:26:46 +0500

    ,

--- app.js
+++ app.js
@@ -71,7 +71,12 @@
 let a = 5;
 let b = 10;
 
+console.log("a = ", a, " & b = ", b,);
 console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a % b =", a % b);
+console.log("a / b =", a / b);
 
 
 
