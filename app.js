163c3a5f4288edc28baa880ce7a27c9759f1c555
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-08-20 04:52:32 +0500

    finally leacture 2 complete

--- app.js
+++ app.js
@@ -246,7 +246,7 @@
 //   } else {
 //     color = "white";
 //   }
-  
+
 //   console.log(color); //  white result
 
 // <<<<<<<, TERNARY OPERATORS >>>>>>>>>>>
@@ -271,7 +271,7 @@
 //     console.log(num, "is a multiple of 5");
 // } else {
 //     console.log(num, "is NOT a multiple of 5");
-// } 
+// }
 
 // let num = prompt("enter a number:");
 
@@ -280,3 +280,20 @@
 // } else {
 //     console.log(num, "is NOT a multiple of 5");
 // }
+
+// let score = prompt("enter your score (0-100):");
+// let grade;
+
+// if (score >= 90 && score <= 100) {
+//   grade = "A";
+// } else if (score >= 70 && score <= 89) {
+//   grade = "B";
+// } else if (score >= 60 && score <= 69) {
+//   grade = "C";
+// } else if (score >= 50 && score <= 59) {
+//   grade = "D";
+// } else if (score >= 0 && score  <= 49) {
+//   grade = "F";
+// }
+
+// console.log(grade);
