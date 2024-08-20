ea289d086862f84be28735e8b04781dfa1f08504
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-08-21 04:20:30 +0500

    for looops

--- app.js
+++ app.js
@@ -297,3 +297,32 @@
 // }
 
 // console.log(grade);
+
+
+///<<<<<<<<< CHAPTER 3 START NOW>>>>>>>>>>
+//   (FOR LOOOOPSSS )
+
+// for (let i = 1; i <= 5; i++)
+// console.log("apna college");
+
+// calculate sum 1 to 5
+// let sum = 0;
+
+// for (let i = 1; i <= 5; i++)
+//     sum = sum + i;
+// console.log("sum, ", sum);
+
+
+// // calculate sum 1 to 100
+// let sum = 0;
+// let n = 100;
+
+// for (let i = 1; i <= n; i++) {
+//     sum = sum + i;
+// }
+// console.log("sum, ", sum);
+
+//   (WHILE LOOOOPSSS )
+
+let i = 1;
+while ()
