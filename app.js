1c91eb58812246ac0a54540d60ab944377cb7621
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-08-23 04:29:55 +0500

    for loops and odd even number

--- app.js
+++ app.js
@@ -380,3 +380,22 @@
 // for (let key in student) {
 //     console.log("key =", key , " value=", student[key]);
 // }
+
+// for (let num = 0; num <= 100; num++) {
+//     console.log("num ", num);
+// }
+
+
+// for (let num = 0; num <= 100; num++) {
+//     if (num % 2 === 0) {
+//         // even number
+//         console.log("num ", num);
+//     }
+
+
+// for (let num = 0; num <= 100; num++) {
+//     if (num % 2 !== 0) {
+//         // odd number
+//         console.log("num ", num);
+//     }
+// }
