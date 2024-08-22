419175238d8e2a9917fbd460afbd18b1f4749bad
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-08-23 03:34:05 +0500

    .

--- app.js
+++ app.js
@@ -339,9 +339,17 @@
 
 // <<<<<<<<<,, DO-WHILEL LOOPS>>>>>...
 
-let i = 20;
-do {
-    
-}
+// let i = 20;
+// do {
+//     console.log("apna college");
+//     i++;
+// } while (i <= 10);
+
+
+// let i = 1;
+// do {
+//     console.log("i= ",i);
+//     i++;
+// } while (i <= 10);
 
 
